ef9894142a91a657b3149c007752f3a492e12be3
ccanokwuru <cccanoks@gmail.com>
2023-11-21 00:28:05 +0100

    color setup

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -1,19 +1,19 @@
-const tintColorLight = '#2f95dc';
-const tintColorDark = '#fff';
+const tintColorLight = "#00009B";
+const tintColorDark = "#cfcfcf";
 
 export default {
   light: {
-    text: '#000',
-    background: '#fff',
+    text: "#00000b",
+    background: "#f8f8f8",
     tint: tintColorLight,
-    tabIconDefault: '#ccc',
+    tabIconDefault: "#ccc",
     tabIconSelected: tintColorLight,
   },
   dark: {
-    text: '#fff',
-    background: '#000',
+    text: "#f8f8f8",
+    background: "#00000b",
     tint: tintColorDark,
-    tabIconDefault: '#ccc',
+    tabIconDefault: "#ccc",
     tabIconSelected: tintColorDark,
   },
 };
